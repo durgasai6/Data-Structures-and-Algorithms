d2dfd36784f2f20c269c810550705e51af9b7f4c
durgasai6 <durgasai.gorantla@gmail.com>
2026-02-17 12:33:56 +0530

    MyDSAWork

---
+++ ProductOfArrayExceptSelf.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    vector<int> answer(n),left(n),right(n);
+    right[n-1]=1;
+    left[0]=1;
+    for(int i=1;i<n;i++){
+        left[i]=left[i-1]*nums[i-1];
+    }
+    for(int i=n-2;i>=0;i--){
+        right[i]=right[i+1]*nums[i+1];
+    }
+    for(int i=0;i<n;i++){
+        answer[i]=left[i]*right[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<answer[i]<<" ";
+    }
+    
+    return 0;
+}
+
+
+/*Given an integer array nums, 
+return an array answer such that answer[i] 
+is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.*/
