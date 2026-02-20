f8972221379c9255821836eb4cf03d89b5c0a1da
durgasai6 <durgasai.gorantla@gmail.com>
2026-02-20 15:48:07 +0530

    MyDSAWork

---
+++ NonDecreasingArray.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int count=0;
+    for(int i=1;i<n;i++){
+        if(nums[i]<nums[i-1]){
+            count++;
+            if(count>1){
+                cout<<"false"<<endl;
+                return 0;
+            }
+            if(i==1 || nums[i-2]<=nums[i]){
+                nums[i-1]=nums[i];
+            }
+            else{
+                nums[i]=nums[i-1];
+            }
+        }
+    }
+    cout<<"true"<<endl;
+    
+    return 0;
+}
+
+
+/*Given an array nums with n integers, 
+your task is to check if it could become 
+non-decreasing by modifying at most one element.
+We define an array is non-decreasing if nums[i] <= nums[i + 1] holds for every i (0-based) such that (0 <= i <= n - 2).
+
+
+
+Example 1:
+
+Input: nums = [4,2,3]
+Output: true
+Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
+Example 2:
+
+Input: nums = [4,2,1]
+Output: false
+Explanation: You cannot get a non-decreasing array by modifying at most one element.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+-105 <= nums[i] <= 105*/
