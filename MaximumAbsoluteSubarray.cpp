a7c67099ed73d9afe8375be68529d563f3927c0a
durgasai6 <durgasai.gorantla@gmail.com>
2026-02-21 11:14:16 +0530

    MyDSAWork

---
+++ MaximumAbsoluteSubarray.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int currentmax=nums[0],maxsum=nums[0], currentmin=nums[0],minsum=nums[0];
+    for(int i=1;i<n;i++){
+        currentmax=max(nums[i],currentmax+nums[i]);
+        maxsum=max(currentmax,maxsum);
+
+        currentmin=min(nums[i],currentmin+nums[i]);
+        minsum=min(currentmin, minsum);
+    }
+    cout<<max(maxsum, abs(minsum))<<endl;;
+    return 0;
+}
+
+
+/*You are given an integer array nums. 
+The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] 
+is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+Note that abs(x) is defined as follows:
+
+If x is a negative integer, then abs(x) = -x.
+If x is a non-negative integer, then abs(x) = x.
+ 
+
+Example 1:
+
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+Example 2:
+
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104*/
