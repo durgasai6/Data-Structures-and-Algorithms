62f2c199bd8a8b91d804bac0c03dbd254e1ff24d
durgasai6 <durgasai.gorantla@gmail.com>
2026-02-26 18:09:17 +0530

    MyDSAWork

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,target;
+    cin>>n>>target;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int left=0,right=n-1,mid;
+    while(left<=right){
+        mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            cout<<mid<<endl;
+            return 0;
+        }
+        else if (nums[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+    }
+    cout<<left<<endl;
+    return 0;
+}
+
+
+
+/*Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104*/
