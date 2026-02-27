6ca6194ec6c7cbd8b385790a329ae054c8553d35
durgasai6 <durgasai.gorantla@gmail.com>
2026-02-27 10:15:43 +0530

    MyDSAWork

---
+++ MoveZeros.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int left=0;
+    for(int right=0;right<n;right++){
+        if(nums[right]!=0){
+            swap(nums[right],nums[left]);
+            left++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
+
+
+
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1*/
