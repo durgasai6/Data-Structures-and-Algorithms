80408277a1b7f431903230bf94e8670bd315e52c
durgasai6 <durgasai.gorantla@gmail.com>
2026-03-03 18:11:45 +0530

    MyDSAWork

---
+++ DailyTemperatures.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<int> temperatures(n);
+    for(int i=0;i<n;i++){
+        cin>>temperatures[i];
+    }
+    vector<int> answer(n,0);
+    stack<int> st;
+    st.push(0);
+    for(int i=1;i<n;i++){
+        while(!st.empty() && temperatures[i]>temperatures[st.top()]){
+            int prev=st.top();
+            st.pop();
+            answer[prev]=i-prev;
+        }
+        st.push(i);
+    }
+    for(int i=0;i<n;i++){
+        cout<<answer[i]<<" ";
+    }
+    return 0;
+}
