a9447b61fbab7ea853d8f2adfaf9ed14b276788b
durgasai6 <durgasai.gorantla@gmail.com>
2026-03-11 16:30:17 +0530

    DSA

---
+++ NumberOfSubarraysOfSizeKAndAverageGreaterThanOrEqualToThreshold.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,k,threshold;
+    cin>>n>>k>>threshold;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int average=0,sum=0,ans=0;
+    int left=0, right=k;
+    for(int i=0;i<k;i++){
+        sum+=nums[i];
+    }
+    if(sum>=threshold*k){
+        ans++;
+    }
+    
+    while(right<n){
+        sum-=nums[left];
+        left++;
+        sum+=nums[right];
+        right++;
+
+        if(sum>=threshold*k){
+            ans++;
+        }
+        
+    }
+    cout<<ans<<endl;
+    
+    
+   
+    return 0;   
+}
+
+
+
+
+
+
+
+
+/*
+Given an array of integers arr and two integers k and threshold, return the number of sub-arrays of size k and average greater than or equal to threshold.
+
+ 
+
+Example 1:
+
+Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+Output: 3
+Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and 6 respectively. All other sub-arrays of size 3 have averages less than 4 (the threshold).
+Example 2:
+
+Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+Output: 6
+Explanation: The first 6 sub-arrays of size 3 have averages greater than 5. Note that averages are not integers.
+ 
+
+Constraints:
+
+1 <= arr.length <= 105
+1 <= arr[i] <= 104
+1 <= k <= arr.length
+0 <= threshold <= 104*/
