abd82ce41b8ac146ccffb8699c3c8ec3ac4826ab
durgasai6 <durgasai.gorantla@gmail.com>
2026-03-14 11:18:42 +0530

    DSA

---
+++ MaximumAverageSubarray1.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,k;
+    cin>>n>>k;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int sum=0;
+    for(int i=0;i<k;i++){
+        sum+=nums[i];
+    }
+    int maxsum=sum;
+    for(int i=k;i<n;i++){
+        sum+=nums[i];
+        sum-=nums[i-k];
+        maxsum=max(maxsum,sum);
+    }
+    double avg=(double)maxsum/k;
+    cout<<fixed<<setprecision(5)<<avg<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+Example 2:
+
+Input: nums = [5], k = 1
+Output: 5.00000
+ 
+
+Constraints:
+
+n == nums.length
+1 <= k <= n <= 105
+-104 <= nums[i] <= 104*/
