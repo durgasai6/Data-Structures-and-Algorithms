1274c1731c8567f1a77902cfc3d9f616961fcb91
durgasai6 <durgasai.gorantla@gmail.com>
2026-03-24 18:13:12 +0530

    DSA

---
+++ RemoveSubFoldersFromTheFileSystem.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<string> folders(n);
+    for(int i=0;i<n;i++){
+        cin>>folders[i];
+    }
+    sort(folders.begin(),folders.end());
+    vector<string> ans;
+    ans.push_back(folders[0]);
+    for(int i=1;i<n;i++){
+        string lastadded=ans.back();
+        if(folders[i].size()>lastadded.size() && folders[i].substr(0,lastadded.size()) == lastadded && folders[i][lastadded.size()]=='/'){
+            continue;
+        }
+        ans.push_back(folders[i]);
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<"  ";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+/*Given a list of folders folder, return the folders after removing all sub-folders in those folders. You may return the answer in any order.
+
+If a folder[i] is located within another folder[j], it is called a sub-folder of it. A sub-folder of folder[j] must start with folder[j], followed by a "/". For example, "/a/b" is a sub-folder of "/a", but "/b" is not a sub-folder of "/a/b/c".
+
+The format of a path is one or more concatenated strings of the form: '/' followed by one or more lowercase English letters.
+
+For example, "/leetcode" and "/leetcode/problems" are valid paths while an empty string and "/" are not.
+ 
+
+Example 1:
+
+Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+Output: ["/a","/c/d","/c/f"]
+Explanation: Folders "/a/b" is a subfolder of "/a" and "/c/d/e" is inside of folder "/c/d" in our filesystem.
+Example 2:
+
+Input: folder = ["/a","/a/b/c","/a/b/d"]
+Output: ["/a"]
+Explanation: Folders "/a/b/c" and "/a/b/d" will be removed because they are subfolders of "/a".
+Example 3:
+
+Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+ 
+
+Constraints:
+
+1 <= folder.length <= 4 * 104
+2 <= folder[i].length <= 100
+folder[i] contains only lowercase letters and '/'.
+folder[i] always starts with the character '/'.
+Each folder name is unique.*/
