6a5471b2e7c33ff541423a687ca4b51e75fc2df5
durgasai6 <durgasai.gorantla@gmail.com>
2026-03-31 16:01:16 +0530

    DSA

---
+++ LC1535.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,k;
+    cin>>n>>k;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int wins=0;
+    int current=nums[0];
+    for(int i=1;i<n;i++){
+        if(nums[i]<current){
+            wins++;
+        }
+        else if(nums[i]>current){
+            current=nums[i];
+            wins=1;
+        }
+        if(wins==k){
+            cout<<current<<endl;
+            return 0;
+        }
+    }
+    cout<<current<<endl;
+    
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+/*Given an integer array arr of distinct integers and an integer k.
+
+A game will be played between the first two elements of the array (i.e. arr[0] and arr[1]). In each round of the game, we compare arr[0] with arr[1], the larger integer wins and remains at position 0, and the smaller integer moves to the end of the array. The game ends when an integer wins k consecutive rounds.
+
+Return the integer which will win the game.
+
+It is guaranteed that there will be a winner of the game.
+
+ 
+
+Example 1:
+
+Input: arr = [2,1,3,5,4,6,7], k = 2
+Output: 5
+Explanation: Let's see the rounds of the game:
+Round |       arr       | winner | win_count
+  1   | [2,1,3,5,4,6,7] | 2      | 1
+  2   | [2,3,5,4,6,7,1] | 3      | 1
+  3   | [3,5,4,6,7,1,2] | 5      | 1
+  4   | [5,4,6,7,1,2,3] | 5      | 2
+So we can see that 4 rounds will be played and 5 is the winner because it wins 2 consecutive games.
+Example 2:
+
+Input: arr = [3,2,1], k = 10
+Output: 3
+Explanation: 3 will win the first 10 rounds consecutively.
+ 
+
+Constraints:
+
+2 <= arr.length <= 105
+1 <= arr[i] <= 106
+arr contains distinct integers.
+1 <= k <= 109*/
