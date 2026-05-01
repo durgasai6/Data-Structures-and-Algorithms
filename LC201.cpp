74f7612aa39926201c9c0886460e39326c49c350
durgasai6 <durgasai.gorantla@gmail.com>
2026-05-01 11:20:42 +0530

    DSA

---
+++ LC201.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int left,right;
+    cin>>left>>right;
+    while(left<right){
+        right=right&(right-1);
+    }
+    cout<<right<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+/*Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+ 
+
+Example 1:
+
+Input: left = 5, right = 7
+Output: 4
+Example 2:
+
+Input: left = 0, right = 0
+Output: 0
+Example 3:
+
+Input: left = 1, right = 2147483647
+Output: 0
+ 
+
+Constraints:
+
+0 <= left <= right <= 231 - 1*/
