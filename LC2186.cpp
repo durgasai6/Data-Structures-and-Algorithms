fd884672c44e137a44bbdbdde8bfdae52ffc560f
durgasai6 <durgasai.gorantla@gmail.com>
2026-05-01 13:23:45 +0530

    DSA

---
+++ LC2186.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s,t;
+    cin>>s>>t;
+    vector<int> f(26,0);
+    for(char c:s){
+        f[c-'a']++;
+    }
+    for(char c:t){
+        f[c-'a']--;
+    }
+    int count=0;
+    for(int x:f){
+        if(x!=0){
+            count+=abs(x);
+        }
+    }
+    cout<<count<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*You are given two strings s and t. In one step, you can append any character to either s or t.
+
+Return the minimum number of steps to make s and t anagrams of each other.
+
+An anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", t = "coats"
+Output: 7
+Explanation: 
+- In 2 steps, we can append the letters in "as" onto s = "leetcode", forming s = "leetcodeas".
+- In 5 steps, we can append the letters in "leede" onto t = "coats", forming t = "coatsleede".
+"leetcodeas" and "coatsleede" are now anagrams of each other.
+We used a total of 2 + 5 = 7 steps.
+It can be shown that there is no way to make them anagrams of each other with less than 7 steps.
+Example 2:
+
+Input: s = "night", t = "thing"
+Output: 0
+Explanation: The given strings are already anagrams of each other. Thus, we do not need any further steps.
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 2 * 105
+s and t consist of lowercase English letters.*/
