ed2c884e4cfc91dea1e673e9c8e96bec3088dd1b
durgasai6 <durgasai.gorantla@gmail.com>
2026-05-09 10:04:15 +0530

    DSA

--- LC79.cpp
+++ LC79.cpp
@@ -51,7 +51,7 @@ int main(){
 
 
 
-Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+/*Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
@@ -80,4 +80,4 @@ m == board.length
 n = board[i].length
 1 <= m, n <= 6
 1 <= word.length <= 15
-board and word consists of only lowercase and uppercase English letters.
+board and word consists of only lowercase and uppercase English letters.*/
