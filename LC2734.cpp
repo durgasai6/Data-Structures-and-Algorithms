10be7ad79526b9b7973cff1401af1cce320245ed
durgasai6 <durgasai.gorantla@gmail.com>
2026-05-28 11:10:56 +0530

    DSA

---
+++ LC2734.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin>>s;
+    string t=s;
+    int n=s.size();
+    bool changed=false;
+    for(int i=0;i<n;i++){
+        if(s[i]=='a' && changed==false){
+            continue;
+        }
+        else if(s[i]=='a' && changed==true){
+            break;
+        }
+        else{
+            s[i]--;
+            changed=true;
+        }
+            
+    }
+    if(t==s){
+        s[n-1]='z';
+    }
+    cout<<s<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*Given a string s consisting of lowercase English letters. Perform the following operation:
+
+Select any non-empty substring then replace every letter of the substring with the preceding letter of the English alphabet. For example, 'b' is converted to 'a', and 'a' is converted to 'z'.
+Return the lexicographically smallest string after performing the operation.
+
+ 
+
+Example 1:
+
+Input: s = "cbabc"
+
+Output: "baabc"
+
+Explanation:
+
+Perform the operation on the substring starting at index 0, and ending at index 1 inclusive.
+
+Example 2:
+
+Input: s = "aa"
+
+Output: "az"
+
+Explanation:
+
+Perform the operation on the last letter.
+
+Example 3:
+
+Input: s = "acbbc"
+
+Output: "abaab"
+
+Explanation:
+
+Perform the operation on the substring starting at index 1, and ending at index 4 inclusive.
+
+Example 4:
+
+Input: s = "leetcode"
+
+Output: "kddsbncd"
+
+Explanation:
+
+Perform the operation on the entire string.
+
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists of lowercase English letters*/
