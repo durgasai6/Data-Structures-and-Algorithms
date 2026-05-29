d66816264393cdb8f0ce7cd62f5774b48a597826
durgasai6 <durgasai.gorantla@gmail.com>
2026-05-29 13:18:15 +0530

    DSA

---
+++ LC537.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string num1,num2;
+    cin>>num1>>num2;
+    int idx1=num1.find('+');
+    int idx2=num2.find('+');
+
+    int a=stoi(num1.substr(0,idx1));
+    int b=stoi(num1.substr(idx1+1,num1.size()-idx1-2));
+    int c=stoi(num2.substr(0,idx2));
+    int d=stoi(num2.substr(idx2+1,num2.size()-idx2-2));
+    
+    cout<<to_string((a*c)-(b*d))+ "+" + to_string((a*d)+(b*c)) + "i";
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*A complex number can be represented as a string on the form "real+imaginaryi" where:
+
+real is the real part and is an integer in the range [-100, 100].
+imaginary is the imaginary part and is an integer in the range [-100, 100].
+i2 == -1.
+Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+
+ 
+
+Example 1:
+
+Input: num1 = "1+1i", num2 = "1+1i"
+Output: "0+2i"
+Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+Example 2:
+
+Input: num1 = "1+-1i", num2 = "1+-1i"
+Output: "0+-2i"
+Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+ 
+
+Constraints:
+
+num1 and num2 are valid complex numbers.*/
