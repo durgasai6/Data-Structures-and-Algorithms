352e3aeef288bce1ac0a49277a2a2611eb8fef23
durgasai6 <durgasai.gorantla@gmail.com>
2026-06-03 18:17:18 +0530

    DSA

---
+++ LC2807.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=nullptr;
+    }
+};
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    
+    Node* head=nullptr;
+    Node* tail=nullptr;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        Node* newnode=new Node(x);
+        if(head==nullptr){
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+            tail->next=newnode;
+            tail=newnode;
+        }
+    }
+    Node* one=head;
+    Node* two=head->next;
+    while(two!=nullptr){
+        if(one->next==nullptr){
+            break;
+        }
+        Node* temp=new Node(__gcd(one->data, two->data));
+        temp->next=two;
+        one->next=temp;
+        one=two;
+        two=two->next;
+    }
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*Given the head of a linked list head, in which each node contains an integer value.
+
+Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+
+Return the linked list after insertion.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+ 
+
+Example 1:
+
+
+Input: head = [18,6,10,3]
+Output: [18,6,6,2,10,1,3]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+- We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+- We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+- We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+There are no more adjacent nodes, so we return the linked list.
+Example 2:
+
+
+Input: head = [7]
+Output: [7]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+There are no pairs of adjacent nodes, so we return the initial linked list.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5000].
+1 <= Node.val <= 1000*/
