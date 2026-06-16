395dae4f6217cfe636f4f5b1d42e5d8f21bd1837
durgasai6 <durgasai.gorantla@gmail.com>
2026-06-16 14:54:14 +0530

    DSA

---
+++ LC39.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> ans;
+vector<int> path;
+
+void dfs(vector<int> &candidates, int start, int remaining){
+    if(remaining==0){
+        ans.push_back(path);
+        return;
+    }
+    
+    if(remaining<0){
+        return;
+    }
+    for(int i=start;i<candidates.size();i++){
+        path.push_back(candidates[i]);
+        dfs(candidates, i, remaining-candidates[i]);
+        path.pop_back();
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,target;
+    cin>>n>>target;
+    vector<int> candidates(n);
+    for(int i=0;i<n;i++){
+        cin>>candidates[i];
+    }
+    dfs(candidates, 0, target);
+    int a=ans.size();
+    for(int i=0;i<a;i++){
+        int b=ans[i].size();
+        for(int j=0;j<b;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40*/
