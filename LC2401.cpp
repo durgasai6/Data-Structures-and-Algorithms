f215b7a28dc5243402ffd75a9dbdbe9bd351b3f8
durgasai6 <durgasai.gorantla@gmail.com>
2026-07-16 17:27:42 +0530

    DSA

---
+++ LC2401.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestNiceSubarray(vector<int>& nums){
+    int n=nums.size();
+    int left=0,mask=0;
+    int ans=1;
+    for(int right=0;right<n;right++){
+        while((mask&nums[right])!=0){
+            mask^=nums[left];
+            left++;
+        }
+        mask|=nums[right];
+        ans=max(ans,right-left+1);
+    }
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<longestNiceSubarray(nums);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*You are given an array nums consisting of positive integers.
+
+We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+
+Return the length of the longest nice subarray.
+
+A subarray is a contiguous part of an array.
+
+Note that subarrays of length 1 are always considered nice.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,8,48,10]
+Output: 3
+Explanation: The longest nice subarray is [3,8,48]. This subarray satisfies the conditions:
+- 3 AND 8 = 0.
+- 3 AND 48 = 0.
+- 8 AND 48 = 0.
+It can be proven that no longer nice subarray can be obtained, so we return 3.
+Example 2:
+
+Input: nums = [3,1,5,11,13]
+Output: 1
+Explanation: The length of the longest nice subarray is 1. Any subarray of length 1 can be chosen.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109*/
