1886e1851c7a512bf3922a2c344b0eaf14b139e2
durgasai6 <durgasai.gorantla@gmail.com>
2026-07-25 10:35:17 +0530

    DSA

---
+++ LC2225.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> findWinners(vector<vector<int>>& matches){
+    unordered_map<int,int> loss;
+    set<int> players;
+    int n=matches.size();
+    for(int i=0;i<n;i++){
+        players.insert(matches[i][0]);
+        players.insert(matches[i][1]);
+        loss[matches[i][1]]++;
+    }
+    vector<vector<int>> ans(2);
+    for(int x:players){
+        if(loss[x]==0){
+            ans[0].push_back(x);
+        }
+        else if(loss[x]==1){
+            ans[1].push_back(x);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector<vector<int>> matches(n,vector<int>(2));
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<2;j++){
+            cin>>matches[i][j];
+        }
+    }
+
+    vector<vector<int>> ans=findWinners(matches);
+    int a=ans.size();
+    for(int i=0;i<a;i++){
+        int b=ans[i].size();
+        for(int j=0;j<b;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+Return a list answer of size 2 where:
+
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note:
+
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+ 
+
+Example 1:
+
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+Example 2:
+
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+ 
+
+Constraints:
+
+1 <= matches.length <= 105
+matches[i].length == 2
+1 <= winneri, loseri <= 105
+winneri != loseri
+All matches[i] are unique.*/
