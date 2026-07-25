5a7d2d62a785237ef74ab89144d31ddc6cb6bc94
durgasai6 <durgasai.gorantla@gmail.com>
2026-07-25 10:16:28 +0530

    DSA

---
+++ LC516.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestPalindromeSubsequence(string s){
+    string t=s;
+    reverse(t.begin(),t.end());
+    int n=s.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s[i-1]==t[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    return dp[n][n];
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin>>s;
+
+    cout<< longestPalindromeSubsequence(s);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: s = "bbbab"
+Output: 4
+Explanation: One possible longest palindromic subsequence is "bbbb".
+Example 2:
+
+Input: s = "cbbd"
+Output: 2
+Explanation: One possible longest palindromic subsequence is "bb".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists only of lowercase English letters.*/
